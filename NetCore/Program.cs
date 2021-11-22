a4b35832e817e6e9612cd66b7b1b61c78e8504af
wangxing <354656790@qq.com>
2021-11-22 14:40:55 +0800

    模板模式

--- NetCore/Program.cs
+++ NetCore/Program.cs
@@ -2,8 +2,6 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-// Start_Prototype s = new Start_Prototype();
-// s.Go();
 
-B_Startup b = new B_Startup();
+Start_Template b = new Start_Template();
 b.Go();
